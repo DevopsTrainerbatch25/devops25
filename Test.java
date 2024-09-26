675489273a091f570b629e4987fc82dd3a294e77
DevopsTrainerbatch25 <gcpdevopsbatch25@gmail.com>
2024-09-26 08:23:01 +0530

    Added addition code

--- Test.java
+++ Test.java
@@ -2,7 +2,10 @@ class Test
 {
 public static void main(String args[])
 {
+  int a=5,b=10,res;
 System.out.println("Hello World");
 System.out.println("Thank you Team");
+res=a+b;
+  System.out.println("Sum of "+a+" and "+b+" is "+res);
 }
 }
